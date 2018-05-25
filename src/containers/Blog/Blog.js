244e99cc44baae9fd26e70ff4574e1fb9a6e1c40
RedLeader12 <redleader@jigsaw.xyz>
2018-05-25 08:24:40 +0800

    Use the Switch route in Blog to make sure one component gets rendered. Also learnt how to navigate programatically without the Link feature from react-router-dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Route,  NavLink }from 'react-router-dom'
+import { Route,  NavLink, Switch }from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost'
 
 
 class Blog extends Component {
@@ -31,8 +32,11 @@ class Blog extends Component {
                         </ul> 
                     </nav>
                 </header>
+                <Switch> 
                     <Route path="/" exact component={Posts}/> 
-                    <Route path="/newPost" exact component={NewPost}/>
+                    <Route path="/newPost" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/> 
+                </Switch> 
             </div>
         );
     }
